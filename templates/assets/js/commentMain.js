c658acd5db8c4da75a02b3e30ad9c69da6693ae9
mtbvang <mtb.vang@gmail.com>
2015-07-05 19:06:02 +0200

    Added wrapper around component render calls to work with react 0.13.*.

--- templates/assets/js/commentMain.js
+++ templates/assets/js/commentMain.js
@@ -1,56 +1,56 @@
 requirejs.config({
-    paths: {
-      'react': '/vendor/react/react-with-addons',
-      'jquery': '/vendor/jquery/dist/jquery',
-      'jquery.timeago': '/vendor/jquery-timeago/jquery.timeago',  
-      'showdown': '/vendor/showdown/compressed/Showdown',  
-      'bootstrap': '/vendor/bootstrap/dist/js/bootstrap',
-      'jsx': '/vendor/requirejsx/jsx', //needed if jsx not compiled on server
-      'JSXTransformer': '/vendor/react/JSXTransformer',
-      'app': '/js'
-    },
+	paths : {
+		'react' : '/vendor/react/react-with-addons',
+		'jquery' : '/vendor/jquery/dist/jquery',
+		'jquery.timeago' : '/vendor/jquery-timeago/jquery.timeago',
+		'showdown' : '/vendor/showdown/compressed/Showdown',
+		'bootstrap' : '/vendor/bootstrap/dist/js/bootstrap',
+		'jsx' : '/vendor/requirejsx/jsx', // needed if jsx not compiled on server
+		'JSXTransformer' : '/vendor/react/JSXTransformer',
+		'app' : '/js'
+	},
 
-    shim: {
-        'JSXTransformer': {
-            exports: "JSXTransformer"
-        }, 
-        'jquery.timeago': ["jquery"]
-    }
+	shim : {
+		'JSXTransformer' : {
+			exports : "JSXTransformer"
+		},
+		'jquery.timeago' : [ "jquery" ]
+	}
 });
 
-require(['jquery', 'react', 'app/CommentForm', 'app/CommentList'], 
-  function ($, React, CommentForm, CommentList) {
+require([
+		'jquery',
+		'react',
+		'app/CommentForm',
+		'app/CommentList' ], function($, React, CommentForm, CommentList) {
 
+	$(function whenDomIsReady() {
 
-  $(function whenDomIsReady() {
+		React.render(React.createFactory(CommentForm)({
+			url : '/comment'
+		}), document.getElementById('commentForm'));
 
-      React.renderComponent(
-        CommentForm({url: '/comment'}),
-        document.getElementById('commentForm')
-      );
+		// as soon as this file is loaded, connect automatically,
+		var socket = io.sails.connect();
 
-      // as soon as this file is loaded, connect automatically, 
-      var socket = io.connect();
-      
-      console.log('Connecting to Sails.js...');
+		console.log('Connecting to Sails.js...');
 
-      // Subscribe to updates (a sails get or post will auto subscribe to updates)
-      socket.get('/comment', function (message) {
-        console.log('Listening...' + message);
+		// Subscribe to updates (a sails get or post will auto subscribe to updates)
+		socket.get('/comment', function(message) {
+			console.log('Listening...' + message);
 
-        // initialize the view with the data property
-        React.renderComponent(
-          CommentList({url: '/comment', data:message}),
-          document.getElementById('commentList')
-        );
+			// initialize the view with the data property
+			React.render(React.createFactory(CommentList)({
+				url : '/comment',
+				data : message
+			}), document.getElementById('commentList'));
 
-      });
+		});
 
-      // Expose connected `socket` instance globally so that it's easy
-      // to experiment with from the browser console while prototyping.
-      window.socket = socket;
+		// Expose connected `socket` instance globally so that it's easy
+		// to experiment with from the browser console while prototyping.
+		window.socket = socket;
 
-  });
-  
+	});
 
 });
--- templates/assets/js/todoMain.js
+++ templates/assets/js/todoMain.js
@@ -1,51 +1,56 @@
 requirejs.config({
-    paths: {
-      'react': '/vendor/react/react-with-addons',
-      'app': '/js'
-    },
+	paths : {
+		'react' : '/vendor/react/react-with-addons',
+		'app' : '/js'
+	},
 });
 
-require(['react', 'app/TodoModel', 'app/TodoApp'], 
-  function (React, TodoModel, TodoApp) {
+require([
+		'react',
+		'app/TodoModel',
+		'app/TodoApp' ], function(React, TodoModel, TodoApp) {
 
-  // as soon as this file is loaded, connect automatically, 
-  var socket = io.connect();
+	// as soon as this file is loaded, connect automatically,
+	var socket = io.sails.connect();
 
-  var model = new TodoModel('/todo', socket);
+	var model = new TodoModel('/todo', socket);
 
-  function render() {
+	function render() {
 
-    React.renderComponent(
-      TodoApp({model: model}),
-      document.getElementById('todoapp')
-    );
-  }
+		// React.render(
+		// TodoApp({model: model}),
+		// document.getElementById('todoapp')
+		// );
+		React.render(React.createFactory(TodoApp)({
+			model : model
+		}), document.getElementById('todoapp'));
+	}
 
-  
-  console.log('Connecting to Sails.js...');
+	console.log('Connecting to Sails.js...');
 
-  // Subscribe to updates (a sails get or post will auto subscribe to updates)
-  socket.get('/todo', function (message) {
+	// Subscribe to updates (a sails get or post will auto subscribe to updates)
+	socket.get('/todo', function(message) {
 
-    console.log('Listening...' + JSON.stringify(message));
+		console.log('Listening...' + JSON.stringify(message));
 
-    // initialize the view with the data property
-    model.set(message);
-    model.subscribe(render);
-    render();
+		// initialize the view with the data property
+		model.set(message);
+		model.subscribe(render);
+		render();
 
-  });
+	});
 
-  socket.on('todo', function whenMessageRecevied(message) {
-    console.log('New comet message received :: ', message);
+	socket.on('todo', function whenMessageRecevied(message) {
+		console.log('New comet message received :: ', message);
 
-    // TODO brute force it. should really check the type of message and respond accordingly.
-    // for now we just update the whole list (this requires the mirror option in config/blueprint)
-    socket.get('/todo', function (todos) {
-      model.set(todos);
-    });
+		// TODO brute force it. should really check the type of message and respond
+		// accordingly.
+		// for now we just update the whole list (this requires the mirror option in
+		// config/blueprint)
+		socket.get('/todo', function(todos) {
+			model.set(todos);
+		});
 
-  });
+	});
 
-
-}); //require
+}); // require
